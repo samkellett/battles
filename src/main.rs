21fdcb5b39c00357d0c4bd970e9b9c2b28ff6647
Sam Kellett <samkellett@gmail.com>
2017-03-27 22:49:25 +0100

    Added the event loop back.

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod render;
 use render::materials::MaterialCollection;
 
 fn main() {
-    use glium::DisplayBuild;
+    use glium::{DisplayBuild, Surface};
 
     let display = glium::glutin::WindowBuilder::new()
         .build_glium()
@@ -16,4 +16,20 @@ fn main() {
         let sources = vec![];
         MaterialCollection::new(&display, sources.into_iter());
     };
+
+    // The event loop.
+    loop {
+        let mut target = display.draw();
+        target.clear_color_and_depth((0.99, 0.83, 0.11, 1.0), 1.0);
+
+        target.finish().unwrap();
+
+        for event in display.poll_events() {
+            match event {
+                // The window has been closed.
+                glium::glutin::Event::Closed => return,
+                _ => (),
+            }
+        }
+    }
 }
